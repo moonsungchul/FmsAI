95997e78cbc24261d7b9ca1a27249d0dc29e5ff9
moonstar <moonsuchstar@firemstar.com>
2020-06-21 15:58:04 +0900

    add ai example

--- bin/PennFudanDataset.py
+++ bin/PennFudanDataset.py
@@ -3,6 +3,14 @@ import numpy as np
 import torch
 from PIL import Image
 
+import torchvision
+from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
+from torchvision.models.detection.mask_rcnn import MaskRCNNPredictor
+
+from engine import train_one_epoch, evaluate
+import utils
+import transforms as T
+
 class PennFundanDataset(object):
 
     def __init__(self, root, transforms):
@@ -26,7 +34,7 @@ class PennFundanDataset(object):
         obj_ids = np.unique(mask)
         obj_ids = obj_ids[1:]
 
-        mask = mask == obj_ids[:, None, None]
+        masks = mask == obj_ids[:, None, None]
 
         num_objs = len(obj_ids)
         boxes = []
@@ -62,4 +70,59 @@ class PennFundanDataset(object):
 
     def __len__(self):
         return len(self.imgs)
+
+
+def get_model_instance_segmentation(num_classes):
+    model = torchvision.models.detection.maskrcnn_resnet50_fpn(pretrained=True)
+    in_featrues = model.roi_heads.box_predictor.cls_score.in_features
+    in_features_mask = model.roi_heads.mask_predictor.conv5_mask.in_channels
+    hidden_layer = 256
+    model.roi_heads.mask_predictor = MaskRCNNPredictor(in_features_mask, hidden_layer, num_classes)
+    return model
+
+
+def get_transform(train):
+    transforms = []
+    transforms.append(T.ToTensor())
+    if train:
+        transforms.append(T.RandomHorizontalFlip(0.5))
+    return T.Compose(transforms)
+
+
+def main():
+    device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+    pp = "/home/moonstar/work2/python_work/FmsAI/data/"
+
+    num_classes = 2
+    dataset = PennFundanDataset(pp + "PennFudanPed", get_transform(train=True))
+    dataset_test = PennFundanDataset(pp + "PennFudanPed", get_transform(train=False))
+
+    indices = torch.randperm(len(dataset)).tolist()
+    dataset = torch.utils.data.Subset(dataset, indices[:-50])
+    dataset_test = torch.utils.data.Subset(dataset_test, indices[-50:])
+
+    data_loader = torch.utils.data.DataLoader(dataset, batch_size=2, shuffle=True, num_workers=4, collate_fn=utils.collate_fn)
+    
+    data_loader_test = torch.utils.data.DataLoader(dataset_test, batch_size=1, 
+            shuffle=False, num_workers=4, collate_fn=utils.collate_fn)
+
+
+    model = get_model_instance_segmentation(num_classes)
+    model.to(device)
+
+    params = [ p for p in model.parameters() if p.requires_grad]
+    optimizer = torch.optim.SGD(params, lr=0.005, momentum=0.9, weight_decay=0.0005)
+    lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=3, gamma=0.1)
+
+    num_epochs = 10
+
+    for epoch in range(num_epochs):
+        train_one_epoch(model, optimizer, data_loader, device, epoch, print_freq=10)
+        lr_scheduler.step()
+        evaluate(model, data_loader_test, device=device)
+    print("That's it!")
+
+
+if __name__ == '__main__':
+    main()
 
